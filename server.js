d4606985071ff474a22b5dee2550de0a7d903181
elias <elias.m.alvarez@gmail.com>
2021-03-20 18:30:06 -0300

    2

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 app.use(express.static(__dirname + '/angularapp'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res){
-res.sendFile(path.join(__dirname + '/angularapp/index.html'));
+//res.sendFile(path.join(__dirname + '/angularapp/index.html'));
+res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 console.log('Build successful!!');
