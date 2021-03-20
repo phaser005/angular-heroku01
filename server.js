d1c665541b7adf113e32cd59e9422df59a7741ab
elias <elias.m.alvarez@gmail.com>
2021-03-20 18:58:12 -0300

    4

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-//app.use(express.static(__dirname + '/angularapp'));
-app.use(express.static(__dirname + '/dist/angular-heroku01'));
+app.use(express.static(__dirname + '/angularapp'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res){
-//res.sendFile(path.join(__dirname + '/angularapp/index.html'));
-res.sendFile(path.join(__dirname + '/dist/angular-heroku01/index.html'));
+res.sendFile(path.join(__dirname + '/angularapp/index.html'));
 });
 console.log('Build successful!!');
