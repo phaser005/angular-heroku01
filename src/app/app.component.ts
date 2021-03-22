2ee48126004378bccbb68dec4b9d195347e1fd8b
elias <elias.m.alvarez@gmail.com>
2021-03-21 21:08:07 -0300

    ejercicio1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-heroku01';
+
+  changeTitle()
+  {
+    this.title="I am your new title. Hello!";
+  }
+  calculate()
+  {
+    let myAge;
+    let momsAge;
+    var sum = 0;
+    var average =0;
+    var isOk = false;
+    var divResult = document.getElementById("ageResult");
+    myAge = parseInt((<HTMLInputElement>document.getElementById("myAge")).value);
+    momsAge = parseInt((<HTMLInputElement>document.getElementById("momsAge")).value);
+
+    if(typeof(myAge)=='number' && typeof(momsAge)=='number')
+    {
+      sum = (myAge + momsAge);
+      average = sum /2;
+      isOk = true;
+    }
+
+    if(isOk && divResult !== null)
+    {
+      divResult.style.visibility = "visible";
+      ((<HTMLTextAreaElement>document.getElementById("av")).innerText="This is the average of your ages: " + average.toString());
+      ((<HTMLTextAreaElement>document.getElementById("sum")).innerText="This is the sum of your ages: " + sum.toString());
+
+    }
+
+  }
 }
